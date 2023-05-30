8fdd744d189a686522e8611a089698a0c1ce7324
skkzsh <skkzsh12@gmail.com>
2023-05-30 23:28:11 +0900

    SimpleSequentialChain を使わず, promptを日本語にして対応

--- langchain_summarize_epub.py
+++ langchain_summarize_epub.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 
 from langchain import PromptTemplate
-from langchain.chains import LLMChain, SimpleSequentialChain
 from langchain.chains.summarize import load_summarize_chain
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import UnstructuredEPubLoader
@@ -16,37 +15,42 @@ docs = loader.load_and_split()
 
 llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
-summary_chain = load_summarize_chain(
-    llm,
-    chain_type="map_reduce",
-    # return_intermediate_steps=True,
-    # verbose=True,
-)
-# summary = summary_chain.run(docs)
+map_template = """あなたはプロのライターで、内容を網羅しながらも分かりやすく要約することが得意です。
+以下は書籍の内容の一部です。1500文字以内で要約してください。:
 
-template = """
-以下の文章を日本語にしてください。
+########
 {text}
-"""
+########
 
-prompt_template = PromptTemplate(
-    template=template,
+要約:"""
+
+combine_template = """あなたはプロのライターで、内容を網羅しながらも分かりやすく要約することが得意です。
+1つの書籍を分割して、分割した内容に対して、それぞれ要約した結果を以下に順に並べています。この書籍を1000~1500文字で要約してください。:
+
+########
+{text}
+########
+
+書籍の要約:"""
+
+map_prompt = PromptTemplate(
+    template=map_template,
     input_variables=["text"],
 )
 
-ja_chain = LLMChain(
-    llm=llm,
-    prompt=prompt_template,
-    # return_intermediate_steps=True,
+combine_prompt = PromptTemplate(
+    template=combine_template,
+    input_variables=["text"],
+)
+
+ja_summary_chain = load_summarize_chain(
+    llm,
+    chain_type="map_reduce",
+    map_prompt=map_prompt,
+    combine_prompt=combine_prompt,
     # verbose=True,
 )
 
-ja_summary_chain = SimpleSequentialChain(
-    chains=[summary_chain, ja_chain],
-    # return_intermediate_steps=True,
-    # verbose=True
-)
+ja_summary = ja_summary_chain.run(docs)
 
-summary = ja_summary_chain.run(docs)
-
-print(summary)
+print(ja_summary)
